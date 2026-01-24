d3cd60654026f0fca766c6790390c372fb337df7
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-01-24 14:21:50 +0530

    Update swap_variables.py

--- swap_variables.py
+++ swap_variables.py
@@ -1,7 +1,19 @@
-# Swap two variables
+# Swap two variables using Python tuple unpacking
 
 a = 5
 b = 10
 
 a, b = b, a
+print(a, b)
+
+
+# Swap two variables using arithmetic operations
+
+a = 5
+b = 10
+
+a = a + b
+b = a - b
+a = a - b
+
 print(a, b)
