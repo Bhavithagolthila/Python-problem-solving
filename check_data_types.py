cdf9c956d34b0c8dce6bdc2e9c9381f32c389162
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-01-26 20:25:31 +0530

    Create check_data_types.py

---
+++ check_data_types.py
@@ -0,0 +1,11 @@
+# Check basic data types in Python
+
+a = 10          # int
+b = 3.5         # float
+c = "Python"    # string
+d = True        # boolean
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
