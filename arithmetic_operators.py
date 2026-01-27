72dc1284b617f54fba024fc15e318fb464884b99
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-01-27 11:21:12 +0530

    Create arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,12 @@
+# Arithmetic operators in Python
+
+a = 10
+b = 3
+
+print(a + b)   # addition
+print(a - b)   # subtraction
+print(a * b)   # multiplication
+print(a / b)   # division
+print(a % b)   # modulus
+print(a // b)  # floor division
+print(a ** b)  # power
