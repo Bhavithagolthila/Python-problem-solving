ba6f5368d0f39034e1f5b3ae682158aa4013779b
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-01-27 21:47:23 +0530

    Create logical_or.py

---
+++ logical_or.py
@@ -0,0 +1,7 @@
+# Program to demonstrate logical OR operator
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# OR operator: any one condition must be true
+print(a > 0 or b > 0)
