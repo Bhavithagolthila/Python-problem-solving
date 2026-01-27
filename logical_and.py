d5e4a88da3e5bc3b4a5ec817ffed46685f8a0912
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-01-27 21:46:47 +0530

    Create logical_and.py

---
+++ logical_and.py
@@ -0,0 +1,7 @@
+# Program to demonstrate logical AND operator
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# AND operator: both conditions must be true
+print(a > 0 and b > 0)
