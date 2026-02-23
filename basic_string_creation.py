2ac0ce1be797ced30b83fb73a06137089d2a2f56
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-02-23 15:13:20 +0530

    Create basic_string_creation.py

---
+++ basic_string_creation.py
@@ -0,0 +1,26 @@
+# Creating Strings in Python
+
+# Using single quotes
+s1 = 'GfG'
+
+# Using double quotes
+s2 = "GfG"
+
+print("Single quote string:", s1)
+print("Double quote string:", s2)
+
+# Using triple quotes for multi-line string
+s3 = '''Hello
+This is
+Multi-line String'''
+
+print("Triple quote string:")
+print(s3)
+
+# String with single quote inside
+s4 = "I'm learning Python"
+print(s4)
+
+# String with double quote inside
+s5 = 'He said "Python is easy"'
+print(s5)
