9480bcd35fe935099a77f3dca1c6f43af9a3f990
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-02-23 15:18:40 +0530

    Create string_indexing.py

---
+++ string_indexing.py
@@ -0,0 +1,11 @@
+# Accessing Characters in String
+
+s = "GeeksforGeeks"
+
+# Positive indexing
+print("First character:", s[0])
+print("Fifth character:", s[4])
+
+# Negative indexing
+print("Third character from end:", s[-3])
+print("Fifth character from end:", s[-5])
