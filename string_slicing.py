a8aa21dedb3ef745d50b9931667a454148a20478
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-02-23 15:28:34 +0530

    Create string_slicing.py

---
+++ string_slicing.py
@@ -0,0 +1,15 @@
+# String Slicing in Python
+
+s = "GeeksforGeeks"
+
+# Slicing with start and end
+print("Index 1 to 3:", s[1:4])
+
+# From beginning to index 2
+print("Start to index 2:", s[:3])
+
+# From index 3 to end
+print("Index 3 to end:", s[3:])
+
+# Reverse string
+print("Reversed string:", s[::-1])
