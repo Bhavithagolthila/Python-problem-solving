ea947af0b430fd36311a5dd319695441df7e7649
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-02-24 14:38:18 +0530

    Create escape_sequences.py

---
+++ escape_sequences.py
@@ -0,0 +1,22 @@
+# Escape Sequence Characters in Python
+
+# New line
+print("Hello\nWorld")
+
+# Tab space
+print("Hello\tWorld")
+
+# Backslash
+print("This is a backslash: \\")
+
+# Single quote inside string
+print('I\'m learning Python')
+
+# Double quote inside string
+print("He said \"Python is easy\"")
+
+# Carriage return
+print("Hello\rWorld")
+
+# Backspace
+print("Helloo\b")
