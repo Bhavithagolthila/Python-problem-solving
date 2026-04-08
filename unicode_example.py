29f30969cdc2646c6dea707d5f689be501a985d5
Bhavitha G <126308514+Bhavithagolthila@users.noreply.github.com>
2026-04-08 21:41:23 +0530

    Create unicode_example.py

---
+++ unicode_example.py
@@ -0,0 +1,19 @@
+# Unicode Example in Python
+
+# Convert characters to Unicode values using ord()
+
+a = "A"
+b = "a"
+c = "Z"
+
+print("Unicode of A:", ord(a))
+print("Unicode of a:", ord(b))
+print("Unicode of Z:", ord(c))
+
+# Convert Unicode values to characters using chr()
+
+x = 65
+y = 66
+
+print("Character of 65:", chr(x))
+print("Character of 66:", chr(y))
